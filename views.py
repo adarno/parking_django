bb74f12845ee0f8f64cd8707a7d741338f69597d
adarno <37181492+adarno@users.noreply.github.com>
2019-06-21 15:40:00 +0200

    add variables

--- views.py
+++ views.py
@@ -3,22 +3,28 @@ from django.http import HttpResponse
 #from mysite_parkinglot.forms import LoginForm
 #from django.template import loader
 
+test = 0
+your_spot = 1
+
 def index(request):
 	
-	return render(request, 'polls/index.html')
+	return render_page(request)
 
 
 def familie_click(request):
 	print("Familienparkplatz")
-	return render(request, 'polls/index.html')
+	return render_page(request)
 
 def normal_click(request):
 	print("normaler parkplatz")
-	return render(request, 'polls/index.html')
+	return render_page(request)
 
 def frauenparkplatz_click(request):
 	print("Faruenparkplatz")
-	return render(request, 'polls/index.html')
+	return render_page(request)
+
+def render_page(request):
+	return render(request, 'polls/index.html', {"number_spots" : test, "your_spot" : your_spot})
 
 
 # Create your views here.
